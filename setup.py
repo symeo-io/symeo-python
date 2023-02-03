232b62f6a78453049865a217b01d15d7eb3e6169
pierre.oucif <pierre.oucif@gadz.org>
2023-02-03 17:57:09 +0100

    Some refactoring for the CI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup  # type: ignore
+
+setup(
+    name="symeo-python",
+    version="1.0.0-RC1",
+    packages=find_packages(exclude=["test"]),
+    # install_requires=["google-cloud-logging"],
+    extra_require={},
+    url="https://github.com/symeo-io/symeo-python",
+    author="Symeo.io",
+    author_email="support@symeo.io",
+    description="Symeo python SDK with embedded CLI for conf as code",
+)
